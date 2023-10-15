6a547ab9802cd20acddfbeb7686b53bd9069c572
jaya2 <jaya2004kra@gmail.com>
2023-10-15 22:47:35 +0530

    added challenge 3

---
+++ SheCodesPlus/Week Three/Challenge Three/src/index.js
@@ -0,0 +1,8 @@
+let price = 4.5;
+console.log(Math.round(price));
+console.log(Math.ceil(price));
+console.log(Math.floor(price));
+let min = Math.min(4, 1, 3, 10);
+console.log(min);
+let max = Math.max(4, 1, 3, 10);
+console.log(max);
