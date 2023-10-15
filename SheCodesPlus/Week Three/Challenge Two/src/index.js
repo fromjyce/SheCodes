fe853bb51bd3b3cb368b1f5d77a7e7d00f7b9767
jaya2 <jaya2004kra@gmail.com>
2023-10-15 21:45:48 +0530

    added challenge two files

---
+++ SheCodesPlus/Week Three/Challenge Two/src/index.js
@@ -0,0 +1,13 @@
+let country = "Guinea Bissau";
+country = country.replace(" ", "-");
+console.log(country);
+let city = " Sydney   ";
+city = city.trim();
+console.log(city);
+let place = "School";
+place = place.toUpperCase();
+console.log(place);
+let attraction = "Opera House";
+attraction = attraction.toLowerCase();
+console.log(attraction);
+let script = `<h2>I went to visit the ${attraction} in ${city}, right next to my ${place}. Now, I'm getting ready for my trip to ${country}</h2>`;
